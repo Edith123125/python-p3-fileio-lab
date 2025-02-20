aedf5a7f43e9855e2d86cca11d3aa9f9ead98fe1
Edith Gatwiri <edithgatwiri70@gmail.com>
2025-02-20 15:12:47 +0300

    fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
+import os
+
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)  
 
 def read_file(file_name):
-    pass
+    file_path = f"{file_name}.txt"
+
+    if not os.path.exists(file_path):
+        return "File not found"
+    
+    with open(file_path, "r") as file:
+        return file.read()
+
